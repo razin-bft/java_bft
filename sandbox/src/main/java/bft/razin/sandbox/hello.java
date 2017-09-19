cf661249a6338ecb12120fac7f4894e77e2f59c6
razinkov <e.razinkov@bftcom.com>
2017-09-19 13:32:47 +0300

    1

--- sandbox/src/main/java/bft/razin/sandbox/hello.java
+++ sandbox/src/main/java/bft/razin/sandbox/hello.java
@@ -1,19 +1,35 @@
 package bft.razin.sandbox;
-
+import java.util.Scanner;
 public class Hello {
+
+
   public static void main(String[] args) {
     hello("world");
     hello("user");
     hello("Женя");
 
-    Square s = new Square(5);
-    System.out.println("Площадь квадрата со сторонами " + s.l + " = " + s.area());
 
-    Rectangle r = new Rectangle(4,6);
-    System.out.println("Площадь со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+//    System.out.print("Введите любое целое число от 1 до 10: ");
+ //   Scanner in = new Scanner(System.in);
+  //  int a = in.nextInt();
+  //  System.out.println("Число равно " + (a * a));
+
+
+    print("Java easy to learn!");
+
   }
+  public static void print(String s) {
+
+  }
+
+
+
 
   public static void hello(String somebody){
     System.out.println("Hello, " + somebody + "!");
   }
-}
+};
+
+
+
